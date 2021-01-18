7190793ae83b00b0c2556404aa5f7af3c412113c
kaikeSilva <54440964+kaikeSilva@users.noreply.github.com>
2021-01-18 10:50:59 -0300

    Conceito de estados e visibilidade em flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,54 @@
+import 'package:flutter/material.dart';
 
+main() => runApp(PerguntaApp());
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+  void _responder() {
+    setState(() {
+      _perguntaSelecionada = ++_perguntaSelecionada % 3;
+    });
+
+    print(_perguntaSelecionada);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual a sua cor favorita?',
+      'Qual a sua musica favorita?',
+      'Qual o seu filme favorito?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("ola mundo"),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[_perguntaSelecionada]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: _responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: _responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: _responder,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
+  }
+}
